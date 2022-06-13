34ebc642509e6db3651adc57316580407687cfb0
Aradhya <202051034@iiitvadodara.ac.in>
2022-06-13 08:31:11 +0530

    Add alternative JS

--- random-color/rancl.js
+++ random-color/rancl.js
@@ -7,6 +7,23 @@ btn.addEventListener('click',()=> {
     let g = Math.floor(Math.random()*255)+1;
     let b = Math.floor(Math.random()*255)+1;
     head.innerText=`rgb(${r},${g},${b})`;
-    bd.bgColor=`rgb(${r},${g},${b})`;
+    //bd.bgColor=`rgb(${r},${g},${b})`;
+    //this hardcode's the HTML (in-line)
+    bd.style.backgroundColor=`rgb(${r},${g},${b})`;
+    //well apparently, so does this :)
 })
 
+//Alternatively:
+// btn.addEventListener('click', () =>{
+//     const newColor = randomColor();
+//     head.innerText=newColor;
+//     bd.bgColor=newColor;
+// });
+
+// function randomColor()
+// {
+//     let r = Math.floor(Math.random()*255);
+//     let g = Math.floor(Math.random()*255);
+//     let b = Math.floor(Math.random()*255);
+//     return `rgb(${r},${g},${b})`;
+// }
